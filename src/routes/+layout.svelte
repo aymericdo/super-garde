b3fb0b79d5c55c085a05fcbacbb97e038735ccc1
Aymeric Dominique <aymeric.dominique@gmail.com>
2023-12-09 17:25:06 +0100

    use base

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import '../app.postcss'
 
+  import { base } from '$app/paths';
   import { applyAction, enhance } from '$app/forms'
   import { pb } from '$lib/pocketbase'
   import { currentUser } from '$lib/stores/user'
@@ -19,12 +20,12 @@
 <div class="bg-neutral text-neutral-content">
   <div class="max-w-l mx-auto navbar">
     <div class="navbar-start">
-      <a href="." class="btn btn-ghost text-xl">(Super) logiciel de garde</a>
+      <a href="{base}" class="btn btn-ghost text-xl">(Super) logiciel de garde</a>
 
       {#if $currentUser}
         <div class="hidden w-full lg:flex lg:w-auto">
-          <a href="calendar" class="btn btn-ghost text-l mx-1" class:btn-active={currentRoute === "/calendar"}>Calendrier</a>
-          <a href="students" class="btn btn-ghost text-l mx-1" class:btn-active={currentRoute === "/students"}>Étudiants</a>
+          <a href="{base}/calendar" class="btn btn-ghost text-l mx-1" class:btn-active={currentRoute === base + "/calendar"}>Calendrier</a>
+          <a href="{base}/students" class="btn btn-ghost text-l mx-1" class:btn-active={currentRoute === base + "/students"}>Étudiants</a>
         </div>
       {/if}
     </div>
@@ -46,11 +47,11 @@
 
         <div class:hidden={!isOpen} class="absolute -bottom-1 translate-y-full w-40 right-1 shadow-md sm:rounded-lg bg-slate-100 z-20">
           {#if $currentUser}
-            <a href="calendar" on:click={() => (isOpen = false)}
-                class:btn-active={currentRoute === "/calendar"}
+            <a href="{base}/calendar" on:click={() => (isOpen = false)}
+                class:btn-active={currentRoute === base + "/calendar"}
                 class="btn btn-ghost text-l w-full text-black">Calendrier</a>
-            <a href="students" on:click={() => (isOpen = false)}
-                class:btn-active={currentRoute === "/students"}
+            <a href="{base}/students" on:click={() => (isOpen = false)}
+                class:btn-active={currentRoute === base + "/students"}
                 class="btn btn-ghost text-l w-full text-black">Étudiants</a>
             <form
               class="btn btn-ghost text-l w-full text-black"
@@ -68,10 +69,10 @@
           {:else}
             <a class="btn btn-ghost text-l w-full text-black"
               on:click={() => (isOpen = false)}
-              href="login">Log in</a>
+              href="{base}/login">Log in</a>
             <!-- <a class="btn btn-ghost text-l w-full text-black"
               on:click={() => (isOpen = false)}
-              href="register">Register</a> -->
+              href="{base}/register">Register</a> -->
           {/if}
         </div>
       </div>
@@ -79,7 +80,7 @@
       <div class="hidden w-full lg:flex lg:w-auto">
         <div class="menu menu-horizontal">
           {#if $currentUser}
-            <a class="btn btn-ghost text-l mx-1" href=".">
+            <a class="btn btn-ghost text-l mx-1" href="{base}">
               {$currentUser.email}
               {#if $currentUser.isAdmin}
                 <div class="badge badge-accent">admin</div>
@@ -101,8 +102,8 @@
               <button>Log out</button>
             </form>
           {:else}
-            <a class="btn btn-ghost text-l mx-1" href="login">Login</a>
-            <!-- <a class="btn btn-ghost text-l mx-1" href="register">Register</a> -->
+            <a class="btn btn-ghost text-l mx-1" href="{base}/login">Login</a>
+            <!-- <a class="btn btn-ghost text-l mx-1" href="{base}/register">Register</a> -->
           {/if}
         </div>
       </div>
