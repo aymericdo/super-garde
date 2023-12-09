f9b22d6dd0ed207b7f818c39fb4bb28526e8fc45
aymeric dominique <aymeric.dominique@gmail.com>
2023-12-09 12:39:45 +0100

    try

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,8 @@ import preprocess from 'svelte-preprocess'
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite'
 
+console.log(process.env.NODE_ENV);
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -16,7 +18,7 @@ const config = {
 
   kit: {
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/super-garde' : '',
+      base: '/super-garde',
       relative: false,
     },
     adapter: adapter(),
