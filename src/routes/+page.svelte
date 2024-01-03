2b19dd730c737c3716668974e4e072e353f64553
aymeric dominique <aymeric.dominique@gmail.com>
2024-01-04 00:46:38 +0100

    elfo url

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,5 @@
 
 <p class="flex flex-col items-center">
   Salut, moi c'est Elfo!
-  <img class="w-36" src="/elfo.webp" alt="elfo">
+  <img class="w-36" src="https://static.wikia.nocookie.net/p__/images/0/0e/Elfo.jpg/revision/latest?cb=20200512223124&path-prefix=protagonist" alt="elfo">
 </p>
