bfbaad36c9e1d77c28ab8af351932a9dfb139f05
aymeric dominique <aymeric.dominique@gmail.com>
2024-01-04 00:28:43 +0100

    show email responsive

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,12 +25,24 @@
       {#if $currentUser}
         <div class="hidden w-full lg:flex lg:w-auto">
           <a href="{base}/calendar" class="btn btn-ghost text-l mx-1" class:btn-active={currentRoute === base + "/calendar"}>Calendrier</a>
-          <a href="{base}/students" class="btn btn-ghost text-l mx-1" class:btn-active={currentRoute === base + "/students"}>Étudiants</a>
+          {#if $currentUser?.isAdmin || ['assistant', 'god'].includes($currentUser?.role)}
+            <a href="{base}/students" class="btn btn-ghost text-l mx-1" class:btn-active={currentRoute === base + "/students"}>Étudiants</a>
+          {/if}
         </div>
       {/if}
     </div>
+
     <div class="navbar-end">
-      <div class="lg:hidden relative">
+      <div class="lg:hidden relative flex items-center w-full justify-end">
+        <a class="hidden sm:flex btn btn-ghost text-l mx-1" href="{base}/">
+          {$currentUser.email}
+          {#if $currentUser.isAdmin}
+            <div class="badge badge-accent">admin</div>
+          {:else}
+            <div class="badge badge-secondary">{$currentUser.role}</div>
+          {/if}
+        </a>
+
         <button on:click={() => (isOpen = !isOpen)} type="button" class="btn btn-secondary block text-gray-500 hover:text-white focus:text-white focus:outline-none">
           <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
             {#if isOpen}
@@ -45,14 +57,26 @@
           class:hidden={!isOpen} class="fixed z-10 w-screen h-screen bg-red top-0 left-0">
         </button>
 
-        <div class:hidden={!isOpen} class="absolute -bottom-1 translate-y-full w-40 right-1 shadow-md sm:rounded-lg bg-slate-100 z-20">
+        <div class:hidden={!isOpen} class="absolute -bottom-1 translate-y-full w-40 right-1 shadow-md rounded bg-slate-100 z-20">
           {#if $currentUser}
+            <a href="{base}/" on:click={() => (isOpen = false)}
+              class="sm:hidden btn btn-ghost relative text-l w-full bg-black text-white flex p-1">
+              <p class="truncate">{$currentUser.email}</p>
+              {#if $currentUser.isAdmin}
+                <div class="badge badge-accent absolute bottom-0 right-0 translate-y-1/2">admin</div>
+              {:else}
+                <div class="badge badge-secondary absolute bottom-0 right-0 translate-y-1/2">{$currentUser.role}</div>
+              {/if}
+            </a>
+
             <a href="{base}/calendar" on:click={() => (isOpen = false)}
                 class:btn-active={currentRoute === base + "/calendar"}
                 class="btn btn-ghost text-l w-full text-black">Calendrier</a>
-            <a href="{base}/students" on:click={() => (isOpen = false)}
-                class:btn-active={currentRoute === base + "/students"}
-                class="btn btn-ghost text-l w-full text-black">Étudiants</a>
+            {#if $currentUser?.isAdmin || ['assistant', 'god'].includes($currentUser?.role)}
+              <a href="{base}/students" on:click={() => (isOpen = false)}
+                  class:btn-active={currentRoute === base + "/students"}
+                  class="btn btn-ghost text-l w-full text-black">Étudiants</a>
+            {/if}
             <form
               class="btn btn-ghost text-l w-full text-black"
               method="POST"
@@ -84,7 +108,7 @@
               {$currentUser.email}
               {#if $currentUser.isAdmin}
                 <div class="badge badge-accent">admin</div>
-              {:else} 
+              {:else}
                 <div class="badge badge-secondary">{$currentUser.role}</div>
               {/if}
             </a>
