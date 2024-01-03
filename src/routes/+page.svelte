a2fbac03a8ba63a94dc8eb70e57a710414fe9d0f
aymeric dominique <aymeric.dominique@gmail.com>
2024-01-04 00:49:52 +0100

    naked

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,5 @@
 
 <p class="flex flex-col items-center">
   Salut, moi c'est Elfo!
-  <img class="w-36" src="https://static.wikia.nocookie.net/p__/images/0/0e/Elfo.jpg/revision/latest?cb=20200512223124&path-prefix=protagonist" alt="elfo">
+  <img class="w-36" src="https://cdnb.artstation.com/p/assets/images/images/060/697/439/large/duncan-barbieri-elfo.jpg?1679113331" alt="elfo">
 </p>
