f82039419e6dd06f88b08d4f88b5ad2cc47a2273
aymeric dominique <aymeric.dominique@gmail.com>
2024-01-04 13:14:11 +0100

    migration

---
+++ pb_migrations/1704359791_updated_students.js
@@ -0,0 +1,18 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("9hv7ybjp8kp7lvv")
+
+  collection.listRule = "@request.auth.id != \"\" && (@request.auth.role = 'assistant' || @request.auth.role = 'god')"
+  collection.viewRule = "@request.auth.id != \"\" && (@request.auth.role = 'assistant' || @request.auth.role = 'god')"
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("9hv7ybjp8kp7lvv")
+
+  collection.listRule = "@request.auth.id != \"\""
+  collection.viewRule = "@request.auth.id != \"\""
+
+  return dao.saveCollection(collection)
+})
