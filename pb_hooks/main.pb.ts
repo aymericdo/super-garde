5390fdd63323eb5f1769066467b58abc90f32ce6
Aymeric Dominique <aymeric.dominique@gmail.com>
2025-09-19 21:34:06 +0200

    test

--- pb_hooks/main.pb.ts
+++ pb_hooks/main.pb.ts
@@ -2,9 +2,8 @@
 /// <reference path="../pb_data/types.d.ts" />
 
 onBootstrap((e) => {
+  e.next()
   // eslint-disable-next-line
   const utils = require(`${__hooks}/helpers/utils.js`);
   utils.hello("world");
-
-  e.next()
 })
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,6 @@
 </script>
 
 <p class="flex flex-col items-center">
-  Salut, moi c'est Elfo !
+  Salut, moi c'est Elfo !!
   <img class="w-36" src="https://cdnb.artstation.com/p/assets/images/images/060/697/439/large/duncan-barbieri-elfo.jpg?1679113331" alt="elfo">
 </p>
