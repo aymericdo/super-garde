b974563aec9a2edecc663d9af9c19f620b8f0a57
Aymeric Dominique <aymeric.dominique@gmail.com>
2025-09-23 02:39:34 +0200

    oups

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import { sveltekit } from '@sveltejs/kit/vite'
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
-  // define: {
-  //   'process.env.NODE_ENV': JSON.stringify('production')
-  // }
 }
 
 export default config
